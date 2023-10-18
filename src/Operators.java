c634f4ad8161843677e1b42d54ae15f8e3d58c45
banci <banc.ioana.andreea@gmail.com>
2023-10-18 16:46:37 +0300

    ok2

--- src/Operators.java
+++ src/Operators.java
@@ -52,6 +52,81 @@ public class Operators  {
         }
 
 
+        System.out.println(10*6);
+        System.out.println(10/5);
+
+        int x = 10;
+        System.out.println(++x);
+        System.out.println(--x);
+
+        x = 10;
+        System.out.println(x+=5);
+
+        int y = 7;
+        int z =15;
+        int suma= y+z;
+        if(suma>=10)
+        {
+            System.out.println("suma este mai mare decat 10 ");
+        }
+        else
+        {
+            System.out.println("suma este mai mica decat 10");
+        }
+
+
+        if(suma % 2==0){
+            System.out.println("suma este numar par");
+
+        }
+         else
+        {
+            System.out.println("suma este numar impar");        }
+
+         int var1=10;
+         int var2=12;
+         int var3=5;
+         int var4=20;
+        System.out.println(var1+var2+var3+var4);
+        System.out.println(var1*var2*var3*var4);
+        System.out.println(var4 % var2);
+
+        System.out.println(++var1);
+        System.out.println(++var1);
+        System.out.println(++var1);
+        System.out.println(var1);
+
+        System.out.println(var2-=2);
+        System.out.println(var2-=2);
+        System.out.println(var2);
+
+        boolean cond1 = (var1 + var2 + var3 + var4) >100;
+        System.out.println(cond1);
+        boolean cond2 = (var1*var2*var3*var4)>1000;
+        System.out.println(cond2);
+
+        boolean condSau = cond1 || cond2;
+        if (condSau){
+            System.out.println("una dintre conditii este adevarata!");
+        }
+        System.out.println(condSau);
+
+
+
+        boolean condSi = cond1 && cond2;
+        if (condSi){
+            System.out.println("ambele conditii sunt adevarate");
+        }  else
+        {
+            System.out.println("cel putin una dintre conditii nu este adevarata");
+        }
+        System.out.println(condSi);
+
+
+
+
+
+
 
 
 
